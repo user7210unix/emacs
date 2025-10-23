9a310c0e53f3733ed374fffed1950635621097d9
0000 <user7210@proton.me>
2025-10-23 15:04:21 +0200

    Update init.el

--- init.el
+++ init.el
@@ -1,7 +1,11 @@
+;; Basic UI settings
 (push '(tool-bar-lines . 0) default-frame-alist)
 (push '(width . 80) default-frame-alist)
 (push '(height . 46) default-frame-alist)
+(menu-bar-mode -1)
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
+;; Package setup
 (require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")
@@ -12,122 +16,104 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(swiper dashboard tabbar session pod-mode muttrc-mode mutt-alias markdown-mode initsplit htmlize graphviz-dot-mode folding eproject diminish csv-mode company color-theme-modern browse-kill-ring boxquote bm bar-cursor apache-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Consolas" :foundry "MS  " :slant normal :weight regular :height 150 :width normal)))))
-
+;; Basic editor settings
+(setq initial-scratch-message nil)
+(defalias 'yes-or-no-p 'y-or-n-p)
+(show-paren-mode 1)
+(electric-pair-mode 1)
 (global-hl-line-mode 1)
-
-(when (>= emacs-major-version 28) (fido-vertical-mode))
-
-;; use flex match.
-(setq completion-styles '(flex))
-
-(setq save-abbrevs 'silently)
-
-;;(load-theme 'misterioso t)
-
-(set-background-color "#EEFFF0")
-
+(savehist-mode 1)
 
 ;; Line numbers
-(global-display-line-numbers-mode t)
-(setq display-line-numbers-type t)  ; Changed from 'relative to t
-(setq display-line-numbers-width-start t)
-
-;; Disable line numbers in certain modes
-(dolist (mode '(org-mode-hook
-                term-mode-hook
-                vterm-mode-hook
-                shell-mode-hook
-                eshell-mode-hook
-                treemacs-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+(when (version<= "26.0.50" emacs-version)
+  (global-display-line-numbers-mode t)
+  (dolist (mode '(org-mode-hook
+                  term-mode-hook
+                  shell-mode-hook
+                  eshell-mode-hook))
+    (add-hook mode (lambda () (display-line-numbers-mode 0)))))
 
 ;; Better scrolling
 (setq scroll-margin 8
       scroll-conservatively 100000
       scroll-preserve-screen-position 1)
 
-;; set default tab char's display width to 4 spaces
-;; default is 8
-(setq-default tab-width 4)
+;; Indentation
+(setq-default tab-width 6
+              indent-tabs-mode nil
+              tab-always-indent 'complete)
 
-;; make tab key always call a indent command.
-(setq-default tab-always-indent t)
-
-;; make tab key call indent command or insert tab character, depending on cursor position
-(setq-default tab-always-indent nil)
-
-;; make tab key do indent first then completion.
-(setq-default tab-always-indent 'complete)
-
-;; auto close bracket insertion
-(electric-pair-mode 1)
-
-
-(when (< emacs-major-version 25)
-  (require 'saveplace)
-  (setq-default save-place t))
-
+;; Save place in files
 (when (>= emacs-major-version 25)
   (save-place-mode 1))
 
+;; Desktop save mode
+(desktop-save-mode 1)
+(setq desktop-save t
+      desktop-load-locked-desktop t
+      desktop-restore-frames t
+      desktop-auto-save-timeout 300)
 
+;; Completion with fido-mode (built-in, fast)
+(when (>= emacs-major-version 28)
+  (fido-vertical-mode)
+  (setq completion-styles '(flex)))
 
-;; Remove initial scratch message and "For information about GNU Emacs and the 
-;; GNU system, type C-h C-a"
-(use-package emacs
-  :init
-  (setq initial-scratch-message nil)
-  (defun display-startup-echo-area-message ()
-    (message "")))
-
-;; Allow y/n instead of having to type yes/no
-(use-package emacs
-  :init
-  (defalias 'yes-or-no-p 'y-or-n-p))
-
-;; highlight matching paren
-(show-paren-mode 1)
-
-;; highlight entire expression
-;;(setq show-paren-style 'expression)
-
-;; company package
+;; Company for auto-completion
 (use-package company)
 
-;; save minibuffer history
-(savehist-mode 1)
+;; LSP Mode for Java
+(use-package lsp-mode
+  :commands lsp
+  :hook ((java-mode . lsp)
+         (lsp-mode . lsp-enable-which-key-integration))
+  :config
+  (setq lsp-keymap-prefix "C-c l"
+        lsp-enable-file-watchers nil
+        lsp-headerline-breadcrumb-enable nil
+        lsp-lens-enable nil
+        lsp-signature-auto-activate nil
+        lsp-modeline-code-actions-enable nil))
 
-;; Restore opened files
-(progn
-  (desktop-save-mode 1)
-  ;; save when quit
-  (setq desktop-save t)
-  ;; no ask if crashed
-  (setq desktop-load-locked-desktop t)
+;; LSP UI for better visuals (optional but recommended)
+(use-package lsp-ui
+  :after lsp-mode
+  :commands lsp-ui-mode
+  :config
+  (setq lsp-ui-doc-enable nil
+        lsp-ui-sideline-enable t
+        lsp-ui-sideline-show-diagnostics t
+        lsp-ui-sideline-show-hover nil))
 
-  (setq desktop-restore-frames t)
+;; LSP Java
+(use-package lsp-java
+  :after lsp-mode
+  :config
+  (setq lsp-java-vmargs
+        '("-XX:+UseParallelGC"
+          "-XX:GCTimeRatio=4"
+          "-XX:AdaptiveSizePolicyWeight=90"
+          "-Dsun.zip.disableMemoryMapping=true"
+          "-Xmx2G"
+          "-Xms100m")))
 
-  (setq desktop-auto-save-timeout 300)
+;; Flycheck for syntax checking
+(use-package flycheck
+  :hook (prog-mode . flycheck-mode))
 
-  (setq desktop-globals-to-save nil)
-)
+;; Magit for git
+(use-package magit
+  :bind ("C-x g" . magit-status))
 
+
+;; Modus Vivendi theme (built-in from Emacs 28+)
+(load-theme 'modus-vivendi t)
+
+(custom-set-faces
+ '(default ((t (:family "Consolas" :foundry "MS  " :slant normal :weight regular :height 155 :width normal)))))
+
+(defun display-startup-echo-area-message ()
+  (message ""))
