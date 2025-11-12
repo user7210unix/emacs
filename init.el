58469419a0b2d759863b0a1ea25d6495d648d403
0000 <user7210@proton.me>
2025-11-12 13:26:10 +0100

    Refactor init.el for improved Emacs configuration

--- init.el
+++ init.el
@@ -1,133 +1,163 @@
-(push '(tool-bar-lines . 0) default-frame-alist)
-(push '(width . 80) default-frame-alist)
-(push '(height . 46) default-frame-alist)
+;;; -*- lexical-binding: t; -*-
+;;; init.el --- Emacs styled like a GNOME app (light Inter setup)
 
+;; -------------------------------
+;; FRAME & STARTUP
+;; -------------------------------
+(push '(width  . 80) default-frame-alist)
+(push '(height . 46) default-frame-alist)
+(setq frame-title-format nil
+      inhibit-startup-screen t
+      inhibit-startup-echo-area-message (user-login-name)
+      initial-scratch-message nil
+      ring-bell-function 'ignore)
+
+;; -------------------------------
+;; PACKAGE MANAGEMENT
+;; -------------------------------
 (require 'package)
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+        ("org"   . "https://orgmode.org/elpa/")
+        ("elpa"  . "https://elpa.gnu.org/packages/")))
 (package-initialize)
 
-;; Install use-package
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
-
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+;; -------------------------------
+;; UI APPEARANCE
+;; -------------------------------
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(global-hl-line-mode 1)
+(show-paren-mode 1)
+(electric-pair-mode 1)
+(savehist-mode 1)
 
+;; Smooth scrolling
+(when (fboundp 'pixel-scroll-precision-mode)
+  (pixel-scroll-precision-mode 1))
+(setq scroll-margin 8
+      scroll-conservatively 100000
+      scroll-preserve-screen-position 1)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(swiper dashboard tabbar session pod-mode muttrc-mode mutt-alias markdown-mode initsplit htmlize graphviz-dot-mode folding eproject diminish csv-mode company color-theme-modern browse-kill-ring boxquote bm bar-cursor apache-mode)))
+;; Theme & padding
+(use-package ef-themes
+  :init (load-theme 'ef-light t))
+
+(use-package spacious-padding
+  :init
+  (setq spacious-padding-widths
+        '( :internal-border-width 15
+           :mode-line-width 6
+           :right-divider-width 10
+           :scroll-bar-width 8))
+  (spacious-padding-mode 1))
+
+;; Font — keep your Inter SemiBold
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Consolas" :foundry "MS  " :slant normal :weight regular :height 150 :width normal)))))
+ '(default ((t (:family "Inter" :weight semi-bold :height 128 :width normal)))))
 
-(global-hl-line-mode 1)
+;; -------------------------------
+;; ICONS
+;; -------------------------------
+(use-package nerd-icons)
+(use-package nerd-icons-dired
+  :hook (dired-mode . nerd-icons-dired-mode))
+(use-package nerd-icons-completion
+  :after vertico
+  :config (nerd-icons-completion-mode))
 
-(when (>= emacs-major-version 28) (fido-vertical-mode))
+;; -------------------------------
+;; COMPLETION
+;; -------------------------------
+;; Disable fido, use Vertico instead for GNOME minimal look
+(use-package vertico
+  :init (vertico-mode)
+  :config
+  (setq vertico-cycle t
+        vertico-resize t))
 
-;; use flex match.
-(setq completion-styles '(flex))
+(use-package marginalia
+  :init (marginalia-mode))
 
-(setq save-abbrevs 'silently)
+(use-package orderless
+  :init (setq completion-styles '(orderless basic)
+              completion-category-defaults nil
+              completion-category-overrides '((file (styles basic partial-completion)))))
 
-;;(load-theme 'misterioso t)
+(use-package company
+  :init (global-company-mode))
 
-(set-background-color "#EEFFF0")
+;; -------------------------------
+;; LINE NUMBERS
+;; -------------------------------
+(when (version<= "26.0.50" emacs-version)
+  (global-display-line-numbers-mode t)
+  (dolist (hook '(org-mode-hook term-mode-hook shell-mode-hook eshell-mode-hook))
+    (add-hook hook (lambda () (display-line-numbers-mode 0)))))
 
+;; -------------------------------
+;; SYNTAX CHECKING
+;; -------------------------------
+(use-package flycheck
+  :hook (prog-mode . flycheck-mode))
 
-;; Line numbers
-(global-display-line-numbers-mode t)
-(setq display-line-numbers-type t)  ; Changed from 'relative to t
-(setq display-line-numbers-width-start t)
+;; -------------------------------
+;; LSP CONFIGURATION
+;; -------------------------------
+(use-package lsp-mode
+  :commands lsp
+  :hook ((java-mode . lsp)
+         (lsp-mode . lsp-enable-which-key-integration))
+  :config
+  (setq lsp-keymap-prefix "C-c l"
+        lsp-enable-file-watchers nil
+        lsp-headerline-breadcrumb-enable nil
+        lsp-lens-enable nil
+        lsp-signature-auto-activate nil
+        lsp-modeline-code-actions-enable nil))
 
-;; Disable line numbers in certain modes
-(dolist (mode '(org-mode-hook
-                term-mode-hook
-                vterm-mode-hook
-                shell-mode-hook
-                eshell-mode-hook
-                treemacs-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+(use-package lsp-ui
+  :after lsp-mode
+  :commands lsp-ui-mode
+  :config
+  (setq lsp-ui-doc-enable nil
+        lsp-ui-sideline-enable t
+        lsp-ui-sideline-show-diagnostics t
+        lsp-ui-sideline-show-hover nil))
 
-;; Better scrolling
-(setq scroll-margin 8
-      scroll-conservatively 100000
-      scroll-preserve-screen-position 1)
+(use-package lsp-java)
 
-;; set default tab char's display width to 4 spaces
-;; default is 8
-(setq-default tab-width 4)
-
-;; make tab key always call a indent command.
-(setq-default tab-always-indent t)
-
-;; make tab key call indent command or insert tab character, depending on cursor position
-(setq-default tab-always-indent nil)
-
-;; make tab key do indent first then completion.
-(setq-default tab-always-indent 'complete)
-
-;; auto close bracket insertion
-(electric-pair-mode 1)
-
-
-(when (< emacs-major-version 25)
-  (require 'saveplace)
-  (setq-default save-place t))
-
-(when (>= emacs-major-version 25)
-  (save-place-mode 1))
-
-
-
-;; Remove initial scratch message and "For information about GNU Emacs and the 
-;; GNU system, type C-h C-a"
-(use-package emacs
+;; -------------------------------
+;; GNOME-LIKE MODELINE
+;; -------------------------------
+(use-package mood-line
   :init
-  (setq initial-scratch-message nil)
-  (defun display-startup-echo-area-message ()
-    (message "")))
+  (add-hook 'after-init-hook #'mood-line-mode))
 
-;; Allow y/n instead of having to type yes/no
-(use-package emacs
-  :init
-  (defalias 'yes-or-no-p 'y-or-n-p))
+;; -------------------------------
+;; GNOME-LIKE TABS
+;; -------------------------------
+(tab-bar-mode 1)
+(setq tab-bar-close-button-show nil
+      tab-bar-new-button-show nil
+      tab-bar-format '(tab-bar-format-tabs))
 
-;; highlight matching paren
-(show-paren-mode 1)
-
-;; highlight entire expression
-;;(setq show-paren-style 'expression)
-
-;; company package
-(use-package company)
-
-;; save minibuffer history
-(savehist-mode 1)
-
-;; Restore opened files
-(progn
-  (desktop-save-mode 1)
-  ;; save when quit
-  (setq desktop-save t)
-  ;; no ask if crashed
-  (setq desktop-load-locked-desktop t)
-
-  (setq desktop-restore-frames t)
-
-  (setq desktop-auto-save-timeout 300)
-
-  (setq desktop-globals-to-save nil)
-)
-
+(provide 'init)
+;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
